2bb651d35e30c6d7c769b5ccf212c4a82046efc9
zadealfalah <zadealfalah@gmail.com>
2024-08-31 13:16:35 -0500

    Add pre-commit hooks, update pipeline, format .py
    Add black/flake8 for formatting/checking via pre commit hooks, apply to current .py files as well.

--- src/terraform/modules/lambda/counter.py
+++ src/terraform/modules/lambda/counter.py
@@ -3,45 +3,46 @@ import boto3
 import os
 
 dynamodb = boto3.resource("dynamodb")
-table = dynamodb.Table(os.environ['DYNAMODB_TABLE_NAME']) # Env var in lambda/main.tf
+# Env var in lambda/main.tf
+table = dynamodb.Table(os.environ["DYNAMODB_TABLE_NAME"])
+
 
 def lambda_handler(event, context):
     try:
         # Retrieve the current view count
-        response = table.get_item(Key={'id': '0'})
-        
+        response = table.get_item(Key={"id": "0"})
+
         # Initialize views to 0 if the item does not exist
-        if 'Item' not in response:
+        if "Item" not in response:
             views = 0
         else:
             # Try converting the views to int
-            views = int(response['Item']['views'])
-        
+            views = int(response["Item"]["views"])
+
         # Increment view count
         views += 1
-        
+
         # Update the item in DynamoDB
-        table.put_item(Item={'id': '0', 'views': views})
-        
+        table.put_item(Item={"id": "0", "views": views})
+
         # Return the updated view count
         return {
-            'statusCode': 200,
-            'headers': {
-                'Access-Control-Allow-Headers': 'Content-Type',
-                'Access-Control-Allow-Origin': 'https://zadealfalah.com',
-                'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
-                },
-            # 'body': json.dumps(views) 
-            'body': json.dumps({'visitorCount': views})
+            "statusCode": 200,
+            "headers": {
+                "Access-Control-Allow-Headers": "Content-Type",
+                "Access-Control-Allow-Origin": "https://zadealfalah.com",
+                "Access-Control-Allow-Methods": "OPTIONS,POST,GET",
+            },
+            # 'body': json.dumps(views)
+            "body": json.dumps({"visitorCount": views}),
         }
     except (TypeError, ValueError, KeyError) as e:
         # Handle errors in conversion or accessing item
         print(f"Error processing item: {e}")
         return {
-            'statusCode': 500,
-            'headers': {
-                'Content-Type': 'application/json'
-            },
-            'body': json.dumps({'error': f'Internal server error. {e}.  Views: {views}'})
+            "statusCode": 500,
+            "headers": {"Content-Type": "application/json"},
+            "body": json.dumps(
+                {"error": f"Internal server error. {e}.  Views: {views}"}
+            ),
         }
-    
